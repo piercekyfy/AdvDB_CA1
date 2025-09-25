d7c4b79d0eed373ecc1aa56507e61c9283b5f01f
PWH <piercekfoley@gmail.com>
2025-09-25 17:38:40 +0100

    Fixed all scripts to use new env. TODO: rewrite the mongo queries I lost. Do last couch DB query, continue

--- 1_BasicCRUD/crud.ps1
+++ 1_BasicCRUD/crud.ps1
@@ -1,3 +1,19 @@
+# // POPULATE ENV VARIABLES \\
+$env_file = if (Test-Path './.env') {'./.env'} elseif (Test-Path '../.env') {'../.env'} elseif (Test-Path '../../.env') {'../../.env'} else {throw '.env not found.'}
+Get-Content $env_file | ForEach-Object {
+    if([string]::IsNullOrWhiteSpace($_) -or $_.StartsWith('#')) { return }
+    
+    $parts = $_ -split '=', 2
+
+    if(-not $parts.Length -eq 2) { return }
+
+    $name = $parts[0].Trim()
+    $value = $parts[1].Trim()
+
+    Set-Item "env:$name" $value
+}
+# // --------------------- \\
+
 # Simple function that returns the value corresponding to '$key' in $json
 function Get-Value {
     param ( [string]$json, [string]$key )
@@ -5,45 +21,41 @@ function Get-Value {
     (($json.Trim('{}') -split ',' | Where-Object { $_ -like "`"$key`"*" }) -split ':')[1].Trim('"')     
 }
 
-$user = Read-Host 'Enter username:'
-$pass = Read-Host 'Enter password:'
-
-$url = "http://${user}:${pass}@127.0.0.1:5984/"
+$url = "http://${ENV:COUCHDB_USER}:${ENV:COUCHDB_PASS}@127.0.0.1:5984/"
 
 $db_name = 'phones'
 
 # GET
 Write-Output "`n`t`GET all rows in the database:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_all_docs" | Select-Object -first 10 | Write-Output
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_all_docs" | Select-Object -first 10 | Write-Output
 Write-Output "`t`t..."
 
-$brand_id = 'MyBrandId'
-$post_json_file = 'post.json'
+$post_json_file = "$PSScriptRoot/post.json"
 
 # POST
-Write-Output "`n`t`POST a new brand with devices:`n"
-$post_result = curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'POST', "$url/phones/", '-d', "@$post_json_file"
+Write-Output "`n`t`POST a new devices:`n"
+$post_result = curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'POST', "$url/$db_name/", '-d', "@$post_json_file"
 Write-Output "$post_result"
 
 $post_result_id = Get-Value $post_result 'id'
 $post_result_rev = Get-Value $post_result 'rev'
 
-Write-Output "`n`tGET the newly created brand:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/$post_result_id"
+Write-Output "`n`tGET the newly created device:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/$post_result_id"
 
-$put_json_file = 'put.json'
+$put_json_file = "$PSScriptRoot/put.json"
 
 # PUT
-Write-Output "`n`tPUT an update of the newly created brand:`n"
-$put_result = curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'PUT', "$url/phones/$post_result_id`?rev=$post_result_rev", '-d', "@$put_json_file"
+Write-Output "`n`tPUT an update of the newly created device:`n"
+$put_result = curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'PUT', "$url/$db_name/$post_result_id`?rev=$post_result_rev", '-d', "@$put_json_file"
 Write-Output "$put_result"
 
-Write-Output "`n`tGET the newly updated brand:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/$post_result_id"
+Write-Output "`n`tGET the newly updated device:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/$post_result_id"
 
 $put_result_rev = Get-Value $put_result 'rev'
 
 # DELETE
 
-Write-Output "`n`tDELETE MyBrand document:`n"
-curl.exe '-s', '-X', 'DELETE', "$url/phones/$post_result_id`?rev=$put_result_rev"
+Write-Output "`n`tDELETE device document:`n"
+curl.exe '-s', '-X', 'DELETE', "$url/$db_name/$post_result_id`?rev=$put_result_rev"
--- 2_Queries/couch/mapreduce.ps1
+++ 2_Queries/couch/mapreduce.ps1
@@ -1,44 +1,59 @@
-$user = Read-Host 'Enter username:'
-$pass = Read-Host 'Enter password:'
+# // POPULATE ENV VARIABLES \\
+$env_file = if (Test-Path './.env') {'./.env'} elseif (Test-Path '../.env') {'../.env'} elseif (Test-Path '../../.env') {'../../.env'} else {throw '.env not found.'}
+Get-Content $env_file | ForEach-Object {
+    if([string]::IsNullOrWhiteSpace($_) -or $_.StartsWith('#')) { return }
+    
+    $parts = $_ -split '=', 2
 
-$url = "http://${user}:${pass}@127.0.0.1:5984/"
+    if(-not $parts.Length -eq 2) { return }
+
+    $name = $parts[0].Trim()
+    $value = $parts[1].Trim()
+
+    Set-Item "env:$name" $value
+}
+# // --------------------- \\
+
+
+$url = "http://${ENV:COUCHDB_USER}:${ENV:COUCHDB_PASS}@127.0.0.1:5984/"
 
 $db_name = 'phones'
 
-$brands_views_file = 'brands_views.json'
+$brands_views_file = "$PSScriptRoot/brands_views.json"
 
 Write-Output "`n`t`Create brands views:`n"
-curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'PUT', "$url/phones/_design/brands", '-d', "@$brands_views_file"
+curl.exe '-s', '-H', 'Content-Type: application/json', '-X', 'PUT', "$url/$db_name/_design/brands", '-d', "@$brands_views_file"
 
 # Find one particular document
-Write-Output "`n`t`Find 'Microsoft' document:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/all?key=%22Microsoft%22" # Powershell is weird about escaping "" so I manually url encode it to %22 here
+Write-Output "`n`t`Find Galaxy A60 document:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/models?key=%22Galaxy%20A60%22" # Powershell is weird about escaping "" so I manually url encode it to %22 here
 
 # Find range of documents (using a view that returns lower-case brand names only)
-Write-Output "`n`t`Find brands from 'A' to 'C' in alphabetical order:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/all_lower?startkey=%22a%22&endkey=%22d%22"
+Write-Output "`n`t`Find devices of brands from 'A' to 'C' in alphabetical order:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/all_lower?startkey=%22a%22&endkey=%22d%22" | Select-Object -first 10 | Write-Output
+Write-Output "`t`t..."
 
 # _sum
 Write-Output "`n`t`Summation:`n"
 
-Write-Output "`t`Total sum of devices:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/by_devices" 
+Write-Output "`t`Sum total device specification count:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/sum_specification_count" | Select-Object -first 10 | Write-Output
 
-Write-Output "`t`Sum of devices per brand:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/by_devices?group=true" | Select-Object -first 10 | Write-Output
+Write-Output "`t`Sum total device specification count per brand:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/sum_specification_count?group=true" | Select-Object -first 10 | Write-Output
 Write-Output "`t`t..."
 
 # _count
 Write-Output "`n`t`Counting:`n"
 
-Write-Output "`t`Count of android devices:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/android_devices" 
+Write-Output "`t`Count of devices:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/count_devices" | Select-Object -first 10 | Write-Output
 
-Write-Output "`t`Count of android devices per brand:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/android_devices?group=true" | Select-Object -first 10 | Write-Output
+Write-Output "`t`Count of devices per brand:`n"
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/count_devices?group=true" | Select-Object -first 10 | Write-Output
 Write-Output "`t`t..."
 
 # _stats
 
 Write-Output "`n`t`Statistics of android devices in all brands:`n"
-curl.exe '-s', '-X', 'GET', "$url/phones/_design/brands/_view/android_count?reduce=true" | Select-Object -first 10 | Write-Output
+curl.exe '-s', '-X', 'GET', "$url/$db_name/_design/brands/_view/android_devices?reduce=true" | Select-Object -first 10 | Write-Output
